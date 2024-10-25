d7dea74e7b611bd3cfc9814349fc205efc60d1b0
Cleitonlanga <cleytonlanga.10@gmail.com>
2024-10-25 15:16:53 +0200

    building navbar

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import Product from './models/products.js';
-//import { connect, models } from 'mongoose';
 import { connectDB } from './config/db.js';
-
+import productRoutes from './routes/product.js';
 
 dotenv.config()
 
 const app = express();
+const PORT = process.env.PORT || 5000
 app.use(express.json()) // allows us to accept JSON data in the req.body
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json({sucess: true, data: products})
-
-    }catch(error){
-        console.log("Error in fetching products:", error.message)
-        res.status(500).json({sucess: false, message: "Server Error"})
-    }
-)
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ sucess: false, message: "Please provide all the fields" });
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ sucess: true, data: newProduct })
-    }
-    catch (error) {
-        console.error("Error creating a product", error.message)
-        res.status(500).json({ sucess: false, message: "Server Error" })
-    }
-})
-
-app.delete("/api/products/:id", async(req, res) => {
-    const {id} = req.params
-    try{
-      await Product.findByIdAndDelete(id)
-      res.status(200).json({sucess: true, message: "Product deleted"})
-    }catch(error){
-        res.status(404).json({sucess: false, message: "Product not found"})
-    }
-} )
+app.use("/api/products", productRoutes)
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000")
+    console.log("Server started at http://localhost:", PORT)
 })
